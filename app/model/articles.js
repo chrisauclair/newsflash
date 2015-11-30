6ce28c04af809a0aec426c10d56fa606bfbc4ba2
Chris Auclair <chris.auclair@gmail.com>
2015-11-29 23:24:40 -0500

    Rename model

--- app/model/articles.js
+++ app/model/articles.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var articleSchema = Schema({
-    _creator: { type: Number, ref: 'Creator' },
+    feed_id: { type: Number, ref: 'Feed' },
     url: {type: String, unique: true},
     title: String,
-    summary: String;
-    keywords: [{ type: Schema.Types.ObjectId, ref: 'Keyword'} ],
+    summary: String,
+    // keywords: [{ type: Schema.Types.ObjectId, ref: 'Keyword'} ],
     location: String,
     time : { type: Date, default: Date.now }
 });
---
+++ app/model/feeds.js
@@ -0,0 +1,10 @@
+// model schema for article publishers
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var feedSchema = Schema({
+    feed: { type: String, unique: true },
+    articles: [{ type: Schema.Types.ObjectId, ref: 'Article' }]
+});
+
+module.exports = mongoose.model('Feed', feedSchema);

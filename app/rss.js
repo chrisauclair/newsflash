743bc0b7bedcda93c0274c437c82df9f31884252
Chris Auclair <chris.auclair@gmail.com>
2015-12-05 18:37:23 -0500

    Add RSS comments

--- app/rss.js
+++ app/rss.js
@@ -6,7 +6,9 @@ var articleHelpers = require('./helpers/article');
 var all = require('node-promise').all;
 
 module.exports = (function(){
-    // start with international news for now
+
+    // TODO: branch feeds into categories and store in the Feed model in the database
+    // test feeds of some international news
     var feeds = [
         "http://www.npr.org/rss/rss.php?id=1009",
         "http://rss.nytimes.com/services/xml/rss/nyt/MiddleEast.xml",
@@ -19,24 +21,25 @@ module.exports = (function(){
         "http://feeds.bbci.co.uk/news/world/middle_east/rss.xml"
     ];
 
-    // TODO determine which params need to be passed to Rss object
     var Rss = function() {
 
+        // parse the body of each XML document and post to database
         var parseBody = function(body) {
-            console.log("parse body");
+            // console.log("parse article");
 
-            var itemTotal = body.rss.channel[0].item.length;
+            // create promises for each item to wait for response from save
             var promises = Utils.createPromises(itemTotal);
 
+            // iterate through each item
+            var itemTotal = body.rss.channel[0].item.length;
             for (var i = 0; i < itemTotal; i++) {
-                console.log("parse item");
+                // console.log("parse item");
 
-                var promise = promises[i];
-
+                // get item and content
                 var item = body.rss.channel[0].item[i];
-
                 var content = (item['content:encoded']) ? item['content:encoded'][0] : item.description[0];
 
+                // build body to match Article schema
                 var articleBody = {
                     feed: body.rss.channel[0].title[0],
                     title: item.title[0],
@@ -48,24 +51,31 @@ module.exports = (function(){
                     location: ""
                 };
 
-                articleHelpers.postArticle(articleBody, promise);
+                // save to database
+                articleHelpers.postArticle(articleBody, promises[i]);
             }
 
+            // return promises
             return all(promises);
         }
 
+        // get Rss XML for feed
         var getRss = function(feed, promise) {
 
             var xhr = new XMLHttpRequest();
 
             xhr.onreadystatechange = function() {
+                // check for status
                 if (this.readyState === 4) {
+
+                    // reject with error for incorrect status
                     if (xhr.status !== 200 && xhr.status !== 302) {
                         xhr.handleError("Invalid response: " + xhr.status);
                         promise.reject(err);
                         return;
                     }
 
+                    // convert response to JSON
                     parseString(this.responseText, function(err, body) {
                         if (err) {
                             console.log("reject parse body: ", err);
@@ -73,6 +83,7 @@ module.exports = (function(){
                             return;
                         }
 
+                        // parse and resolve promise
                         parseBody(body).then(function(res) {
                             promise.resolve(res);
                         }, function(err) {
@@ -82,26 +93,26 @@ module.exports = (function(){
                     })
 
                 }
-
-                if (this.readyState === 2) {
-                                    }
             }
 
+            // GET feed
             xhr.open("GET", feed);
             xhr.send();
-
         }
 
-        // public methods of object instance
+        // initiate Rss reader
         Rss.prototype.init = function() {
             console.log("rss init");
 
+            // create promises for feeds
             var promises = Utils.createPromises(feeds.length);
 
+            // get Rss for each feed
             for (var i = 0, n = feeds.length; i < n; i++) {
                 getRss(feeds[i], promises[i]);
             }
 
+            // return all promises
             return all(promises);
         }
     }

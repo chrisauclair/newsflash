6f0a5fe5168e740736b026ed0c51003fd0d27b07
Chris Auclair <chris.auclair@gmail.com>
2015-12-05 19:31:32 -0500

    Cluster comments

--- app/helpers/cluster.js
+++ app/helpers/cluster.js
@@ -3,9 +3,9 @@ var Cluster = require('../model/clusters');
 var Feed = require('../model/feeds');
 
 module.exports = (function() {
+
+    // get cluster data by id
     var getCluster = function(id, promise) {
-        console.log(id);
-        // .populate({feed: 'feed_id', select: 'feed'})
         Cluster.findById(id).populate({path: 'articles', populate: {path: 'feed_id'}}).exec(function(err, res) {
             if (err) return handleError(err, promise);
 
@@ -13,6 +13,7 @@ module.exports = (function() {
         });
     }
 
+    // handle any errors
     function handleError(err, promise) {
         console.log(err);
         if (promise) promise.reject(err);
